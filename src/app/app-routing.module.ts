2b2b3f07289ee5badf4be4a0a1998346262cb611
Karolina Stefańczyk <karolina.m.ste@gmail.com>
2022-02-25 12:05:54 +0100

    logic and styles to all components and new file 404

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
+import { MyPortfolioComponent } from './my-portfolio/my-portfolio.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainPageComponent },
+  { path: 'home', component: MainPageComponent },
+  { path: 'about-me', component: AboutMeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'my-skills', component: MySkillsComponent },
+  { path: 'my-portfolio', component: MyPortfolioComponent },
+  { path: 'nav-bar', component: NavBarComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
