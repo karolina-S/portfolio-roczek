831119079438c6b3add30cb0d7af06ce839cfe0c
Karolina Stefańczyk <karolina.m.ste@gmail.com>
2022-02-25 11:28:26 +0100

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { MyPortfolioComponent } from './my-portfolio/my-portfolio.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    NavBarComponent,
+    ContactComponent,
+    AboutMeComponent,
+    MyPortfolioComponent,
+    MySkillsComponent
   ],
   imports: [
     BrowserModule,
